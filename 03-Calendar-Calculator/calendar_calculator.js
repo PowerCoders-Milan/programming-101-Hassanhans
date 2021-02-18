d87eb8d83999b0f0c31ee3845936749f237372f8
hassanwasswa <assanwasswa@gmail.com>
2021-02-18 14:52:14 +0100

    exercise 2

--- 03-Calendar-Calculator/calendar_calculator.js
+++ 03-Calendar-Calculator/calendar_calculator.js
@@ -1,6 +1,26 @@
 //insert your pseudocode below
 /*
-here
+set the numbers
+create of them
+print and sort them 
 */
 
 //Insert your code below
+var numbers;
+
+function listsGetSortCompare(type, direction) {
+  var compareFuncs = {
+    "NUMERIC": function(a, b) {
+        return Number(a) - Number(b); },
+    "TEXT": function(a, b) {
+        return a.toString() > b.toString() ? 1 : -1; },
+    "IGNORE_CASE": function(a, b) {
+        return a.toString().toLowerCase() > b.toString().toLowerCase() ? 1 : -1; },
+  };
+  var compare = compareFuncs[type];
+  return function(a, b) { return compare(a, b) * direction; }
+}
+
+
+numbers = [1, 9, 3];
+window.alert(numbers.slice().sort(listsGetSortCompare("NUMERIC", 1)));

6feb57edbf246eb2aa8b590185b5a96e49822813
hassanwasswa <assanwasswa@gmail.com>
2021-02-18 12:26:09 +0100

    exercise 1

--- 01-Blockly/blockly.js
+++ 01-Blockly/blockly.js
@@ -1,1 +1,51 @@
 //Insert level 9 code below
+moveForward();
+moveForward();
+moveForward();
+turnLeft();
+moveForward();
+turnRight();
+moveForward();
+while (notDone()) {
+    moveForward();
+  }
+  while (notDone()) {
+    moveForward();
+    turnLeft();
+    moveForward();
+    turnRight();
+  }
+  moveForward();
+moveForward();
+turnLeft();
+while (notDone()) {
+  moveForward();
+}
+while (notDone()) {
+    moveForward();
+    if (isPathLeft()) {
+      turnLeft();
+    }
+  }
+  while (notDone()) {
+    moveForward();
+    if (isPathRight()) {
+      turnRight();
+    }
+  }
+  while (notDone()) {
+    moveForward();
+    if (isPathLeft()) {
+      turnLeft();
+    }
+    if (isPathRight()) {
+      turnRight();
+    }
+  }
+  while (notDone()) {
+    if (isPathForward()) {
+      moveForward();
+    } else {
+      turnLeft();
+    }
+  }

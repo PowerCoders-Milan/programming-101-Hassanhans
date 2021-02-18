e74919771ba0cbf453f57257228e0b46902c5415
hassanwasswa <assanwasswa@gmail.com>
2021-02-18 18:01:24 +0100

    exercise 3

--- 03-Calendar-Calculator/calendar_calculator.js
+++ 03-Calendar-Calculator/calendar_calculator.js
@@ -1,26 +1,31 @@
 //insert your pseudocode below
 /*
-set the numbers
-create of them
-print and sort them 
+Specify the month and the day of the month in two variables
+the variable of the month will contain a name which be the name of the month
+2nd Variable will contain a number which is the day of the month
+Calcualte the days of all the months before the name of the month
+Calculate the requested number by adding of the month with the days from the months before the dsy
+print
+Variable = April
+Variable2 = 4
+Days before = 31 + 28 + 31 = 90
+result = 90+4 = 94
 */
 
 //Insert your code below
-var numbers;
+var Month, Result, Day_of_the_Month, Day;
 
-function listsGetSortCompare(type, direction) {
-  var compareFuncs = {
-    "NUMERIC": function(a, b) {
-        return Number(a) - Number(b); },
-    "TEXT": function(a, b) {
-        return a.toString() > b.toString() ? 1 : -1; },
-    "IGNORE_CASE": function(a, b) {
-        return a.toString().toLowerCase() > b.toString().toLowerCase() ? 1 : -1; },
-  };
-  var compare = compareFuncs[type];
-  return function(a, b) { return compare(a, b) * direction; }
+if (Month == 'January') {
+  Result = 0 + Day;
 }
-
-
-numbers = [1, 9, 3];
-window.alert(numbers.slice().sort(listsGetSortCompare("NUMERIC", 1)));
+if (Month == 'February') {
+  Result = 31 + Day;
+}
+if (Month == 'March') {
+  Result = 59 + Day;
+}
+if (Month == 'April') {
+  Result = 90 + Day;
+}
+if (Month == 'May') {
+  Result = 120 + Day;

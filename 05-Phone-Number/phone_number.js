94f8afa9577974e50b6daba318d7515103e13a07
hassanwasswa <assanwasswa@gmail.com>
2021-02-19 18:00:00 +0100

    execrise5 Phone number

--- 05-Phone-Number/phone_number.js
+++ 05-Phone-Number/phone_number.js
@@ -1,6 +1,23 @@
 //insert your pseudocode below
 /*
-here
+Create a variable to receive the phone number
+clear the variable incase it is not empty
+prompt for the phone number
+store the response in the variable
+validate the stored response if it is the valid phone number
+
 */
 
 //insert your code below
+var Phone_Number, Valid, Store_the_number;
+
+// Describe this function...
+function do_something() {
+  Store_the_number = !''.length;
+  return false ? Phone_Number.indexOf('Valid Number') + 1 : null;
+}
+
+
+Phone_Number = Number(window.prompt('What is the phone number'));
+
+Valid = 0;
